298dd62d00ebc7964f0744e5e4c285b455dd7f75
Zhengxuan Guo <stguoro@126.com>
2023-10-09 11:57:11 +0800

    issue map tasks

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,19 +1,44 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"time"
+)
 
 
 type Coordinator struct {
 	// Your definitions here.
 	files []string
 	nReduce int
+
+	issued map[string]bool
+	mapReqChan chan struct{}
+	mapRespChan chan string
+	mapDoneChan chan string
+}
+
+type Signal struct {}
+
+type Task struct {
+	file string
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) ReqMapTask(args *Signal, reply *Task) error {
+	c.mapReqChan <- struct{}{}
+	file := <-c.mapRespChan
+	reply.file = file
+	return nil
+}
+
+func (c *Coordinator) MarkMapDone(args *Task, reply *Signal) error {
+	c.mapDoneChan <- args.file
+	return nil
+}
 
 //
 // an example RPC handler.
@@ -64,8 +89,17 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
+	// Init coordinator.
 	c.files = files
 	c.nReduce = nReduce
+	c.issued = make(map[string]bool, len(files))
+	for _, f := range files {
+		c.issued[f] = false
+	}
+	c.mapReqChan = make(chan struct{})
+	c.mapRespChan = make(chan string)
+	c.mapDoneChan = make(chan string)
+	go c.coordinate()
 
 	c.server()
 	return &c
@@ -76,6 +110,30 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 // All modification of fields in the Coordinator happen here,
 // this excludes the use of locks.
 //
-func (c *Coordinator) mainLoop() {
-	
+func (c *Coordinator) coordinate() {
+	doneChan := make(map[string]chan struct{}, len(c.files))
+	for _, f := range c.files {
+		doneChan[f] = make(chan struct{})
+	}
+	for {
+		select {
+		case <-c.mapReqChan:
+			for f := range c.issued {
+				if !c.issued[f] {
+					c.issued[f] = true
+					c.mapRespChan <- f
+					go func ()  {
+						select {
+						case <-doneChan[f]:
+						case <-time.After(10 * time.Second):
+							c.issued[f] = false
+						}
+					}()
+					break
+				}
+			}
+		case f := <-c.mapDoneChan:
+			delete(c.issued, f)
+		}
+	}
 }
